0011758c04327a8701ab687e1e9024fca2993e00
ObadaAhmed <aboodey33@gmail.com>
2020-07-12 07:45:11 +0200

    adding files

---
+++ dist/precache-manifest.342c66c40abb317f983666d45d392e01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e98d6252d77f8dbadb99",
+    "url": "/css/app.70be7117.css"
+  },
+  {
+    "revision": "75babb8de7404b9f703047ed93494002",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb9a585815f9007243f7",
+    "url": "/js/about.83da4a4b.js"
+  },
+  {
+    "revision": "e98d6252d77f8dbadb99",
+    "url": "/js/app.cd6c710e.js"
+  },
+  {
+    "revision": "726a38a24682f2dd4a33",
+    "url": "/js/chunk-vendors.3912fd8d.js"
+  },
+  {
+    "revision": "a22fe3d586fdb366eb6ec93e0d89461b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
